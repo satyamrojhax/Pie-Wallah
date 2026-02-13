a5793295b4e5c08f6e9e4db55921b305996d9818
Satyam Rojha <satyamrojhax@gmail.com>
2026-02-13 13:39:01 +0530

    Deleted

--- android/app/src/main/java/com/piewallah/satyamrojhax/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.piewallah.satyamrojhax;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.piewallah.satyamrojhax',
-  appName: 'Pie Wallah',
-  webDir: 'dist'
-};
-
-export default config;
